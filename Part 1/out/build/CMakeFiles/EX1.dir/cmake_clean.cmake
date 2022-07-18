4f3a794f4cfc79684b17a96322e7aeb153c91333
Daironn <blizzard@vp.pl>
2022-07-18 20:48:54 +0200

    adding working cmake

---
+++ Part 1/out/build/CMakeFiles/EX1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EX1.dir/Exercise_1.c.o"
+  "CMakeFiles/EX1.dir/Exercise_1.c.o.d"
+  "EX1"
+  "EX1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/EX1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
